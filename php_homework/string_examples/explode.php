6f59734658165453b9355798fe1d24c5d84472d9
Jennifer A Causing <jac84@afsconnect1.njit.edu>
2016-11-01 13:53:39 -0400

    edited explode.php

---
+++ php_homework/string_examples/explode.php
@@ -0,0 +1,11 @@
+<?php
+
+//split a string by a string into an array
+//keep this in mind for regex
+
+$str = "this string will explode by the space";
+$strArr = explode(' ', $str);
+
+print_r($strArr);
+
+?>
