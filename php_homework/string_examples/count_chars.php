d851ae87ffeb5c4b6ad9240a6fee19c1401c3039
Jennifer A Causing <jac84@afsconnect1.njit.edu>
2016-11-01 13:46:26 -0400

    edited count_chars.php

---
+++ php_homework/string_examples/count_chars.php
@@ -0,0 +1,10 @@
+<?php
+
+//counts number of times a char occurs in a string and can return in various ways
+
+//this example returns all of the chars used in the sentence
+$str = "The quick fox jumped over the lazy brown dog.";
+
+echo count_chars($str, 3);
+
+?>
