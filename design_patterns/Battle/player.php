c02338e1f3856e69aaf21a233d15a3918a7ae1a2
Jennifer A Causing <jac84@afsconnect1.njit.edu>
2016-11-14 19:11:18 -0500

    redoing entire project. created player and enemy class for Battle/

---
+++ design_patterns/Battle/player.php
@@ -0,0 +1,19 @@
+<?php
+
+  public class Player{
+    protected $health;
+    protected $magic;
+    protected $atkDmg;
+
+    function __construct($hp, $mp, $atk){
+      $this-?health = $hp;
+      $this-?magic = $mp;
+      $this->atkDmg = $atk;
+    }
+
+    public function getHealth(){return $this->health;}
+    public function getMagic(){return $this->magic;}
+    public function getAtk(){return $atkDmg;}
+  }
+
+?>
