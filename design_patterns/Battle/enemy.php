d823617907cfb57587c88cfdc076d203b13c1488
Jennifer A Causing <jac84@afsconnect1.njit.edu>
2016-11-14 22:03:01 -0500

    added level variable to player and enemy

--- design_patterns/Battle/enemy.php
+++ design_patterns/Battle/enemy.php
@@ -1,18 +1,21 @@
 <?php
 
   public class Enemy{
+    protected $level;
     protected $health;
     protected $magic;
     protected $atkDmg;
 
 
 
-    function __construct($hp, $mp, $atk){
+    function __construct($lvl, $hp, $mp, $atk){
+      $this->level = $lvl;
       $this->health = $hp;
       $this->magic = $mp;
       $this->atkDmg = $atk;
     }
 
+    public function getLevel(){return $this->level;}
     public function getHealth(){return $this->health;}
     public function getMagic(){return $this->magic;}
     public function getAtk(){return $atkDmg;}
--- design_patterns/Battle/player.php
+++ design_patterns/Battle/player.php
@@ -1,17 +1,20 @@
 <?php
 
   public class Player{
+    protected $level;
     protected $health;
     protected $magic;
     protected $atkDmg;
 
 
-    function __construct($hp, $mp, $atk){
+    function __construct($lvl, $hp, $mp, $atk){
+      $this->level = $lvl;
       $this->health = $hp;
       $this->magic = $mp;
       $this->atkDmg = $atk;
     }
 
+    public function getLevel(){return $this->level;}
     public function getHealth(){return $this->health;}
     public function getMagic(){return $this->magic;}
     public function getAtk(){return $atkDmg;}
