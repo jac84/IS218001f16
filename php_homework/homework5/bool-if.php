2fcfab0855679b179d91246cd841a278a4af7285
Jennifer A Causing <jac84@afsconnect1.njit.edu>
2016-12-08 13:51:13 -0500

    created 4 switch case examples

--- php_homework/homework5/bool-if.php
+++ php_homework/homework5/bool-if.php
@@ -24,4 +24,53 @@ foreach($someVars as $var){
 	echo '<br>';
 }
 
+echo '<br>' . "-----" . '<br>';
+
+echo '<h2>' . "Switch Case Examples" . '</h2>';
+echo '<br>';
+
+foreach($someVars as $var){
+	switch(isset($var)){
+		case TRUE:
+			echo $var . " is a set variable" . '<br>';
+			break;
+		case FALSE:
+			echo $var . " is not set variable" . '<br>';
+			break;
+	}
+}
+echo '<br>' . "---" . '<br>';
+foreach($someVars as $var){
+	switch(empty($var)){
+		case TRUE:
+			echo $var . " is an empty variable" . '<br>';
+			break;
+		case FALSE:
+			echo $var . " has a value" . '<br>';
+			break;
+	}
+}
+echo '<br>' . "---" . '<br>';
+foreach($someVars as $var){
+	switch(is_null($var)){
+		case TRUE:
+			echo $var . " is null" . '<br>';
+			break;
+		case FALSE:
+			echo $var . " is not null" . '<br>';
+			break;
+	}
+}
+echo '<br>' . "---" . '<br>';
+foreach($someVars as $var){
+	switch(isset($var) && empty($var)){
+		case TRUE:
+			echo $var . " is a set variable but empty" . '<br>';
+			break;
+		case FALSE:
+			echo $var . " is not a set variable" . '<br>';
+			break;
+	}
+}
+
 ?>
