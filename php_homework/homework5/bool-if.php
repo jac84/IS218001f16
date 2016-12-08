4267cace3ab849c6018b192599e769c0f7dd71c5
Jennifer A Causing <jac84@afsconnect1.njit.edu>
2016-12-08 13:10:00 -0500

    continued homework 5 with additional examples of empty() and is_null()

--- php_homework/homework5/bool-if.php
+++ php_homework/homework5/bool-if.php
@@ -13,8 +13,31 @@
 }*/
 
 $someVars = array(0, "0", "", "string", FALSE, TRUE, NULL);
+echo "Array used for example: ";
+print_r($someVars);
 
+echo '<br>' . "-----" . '<br>';
+
+echo '<h2>' . "isset() example" . '</h2>' . '<br>';
 foreach($someVars as $var){
 	echo "$value <br> isset(" . $var . ") is: " .  isset($var) . "<br>";
 }
+
+echo '<br>' . "-----" . '<br>';
+
+echo '<h2>' . 'empty() example' . '</h2>' . '<br>';
+foreach($someVars as $var){
+	echo "$value <br> empty(" . $var . ") is: " .  empty($var) . "<br>";
+}
+
+echo '<br>' . "-----" . '<br>';
+
+echo '<h2>' . 'is_null() example' . '</h2>' . '<br>';
+foreach($someVars as $var){
+	echo "$value <br> is_null(" . $var . ") is: " .  is_null($var) . "<br>";
+}
+
+echo '<br>' . "-----" . '<br>';
+
+
 ?>
