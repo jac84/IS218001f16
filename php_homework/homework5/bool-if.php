dc29205bccee59185a67aae2ee855cf13b21c428
Jennifer A Causing <jac84@afsconnect1.njit.edu>
2016-12-08 13:39:40 -0500

    cleaned up bool-if.php file

--- php_homework/homework5/bool-if.php
+++ php_homework/homework5/bool-if.php
@@ -1,22 +1,13 @@
 <?php
 
-/*class ifElseEx{
-	protected $var;
-
-	public function __construct($v){
-		$this->rows = $a;
-	}
-
-	public function ifElseIsset($input){
-		$varIsset = isset(
-	}
-}*/
-
 $someVars = array(0, "0", "", "string", FALSE, TRUE, NULL);
 echo "Array used for example: ";
 print_r($someVars);
 
 echo '<br>' . "-----" . '<br>';
+
+echo '<h2>' . "If/Else Examples" . '</h2>';
+echo '<br>';
 
 foreach($someVars as $var){
 	if(isset($var)){ //example 1
@@ -32,26 +23,5 @@ foreach($someVars as $var){
 	}
 	echo '<br>';
 }
-/*echo '<h2>' . "isset() example" . '</h2>' . '<br>';
-foreach($someVars as $var){
-	echo "$value <br> isset(" . $var . ") is: " .  isset($var) . "<br>";
-}*/
-
-/*echo '<br>' . "-----" . '<br>';
-
-echo '<h2>' . 'empty() example' . '</h2>' . '<br>';
-foreach($someVars as $var){
-	echo "$value <br> empty(" . $var . ") is: " .  empty($var) . "<br>";
-}
-
-echo '<br>' . "-----" . '<br>';
-
-echo '<h2>' . 'is_null() example' . '</h2>' . '<br>';
-foreach($someVars as $var){
-	echo "$value <br> is_null(" . $var . ") is: " .  is_null($var) . "<br>";
-}
-
-echo '<br>' . "-----" . '<br>';*/
-
 
 ?>
