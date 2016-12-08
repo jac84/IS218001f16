b451f744e75ab2a2f29c669a0157b4a0200a1641
Jennifer A Causing <jac84@afsconnect1.njit.edu>
2016-12-08 13:26:24 -0500

    labeled which if statements are which examples

--- php_homework/homework5/bool-if.php
+++ php_homework/homework5/bool-if.php
@@ -19,15 +19,15 @@ print_r($someVars);
 echo '<br>' . "-----" . '<br>';
 
 foreach($someVars as $var){
-	if(isset($var)){
+	if(isset($var)){ //example 1
 		echo "isset(" . $var . ") is true. " . $var . " is a set value.";
-	} elseif (empty($var)){
+	} elseif (empty($var)){ //example 2
 		echo "empty(" . $var . ") is true. " . $var . " is an empty value.";
-	} elseif (is_null($var)){
+	} elseif (is_null($var)){ //example 3
 		echo "is_null(" . $var . ") is true. " . $var . " is null.";
 	}
 	echo '<br>';
-	if(isset($var) && empty($var)){
+	if(isset($var) && empty($var)){ //example 4
 		echo $var . " is either 0 or false." . '<br>';
 	}
 	echo '<br>';
