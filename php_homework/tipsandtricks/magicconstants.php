08bacd668af8c9a39cb350115239d7a9d4e98d21
Jennifer A Causing <jac84@afsconnect1.njit.edu>
2016-12-12 09:17:11 -0500

    made __LINE__ example in magicconstants.php

---
+++ php_homework/tipsandtricks/magicconstants.php
@@ -0,0 +1,17 @@
+<?php
+
+echo "This line of code is on line ". __LINE__; //should return line 3
+
+/*
+PHP Constants:
+__LINE__ -> current line number
+__FILE__ -> full path and filename. can be used in an include
+__DIR__ -> directory of a file
+__FUNCTION__ -> function name
+__CLASS__ -> class name
+__TRAIT__ -> trait name
+__METHOD__ -> class method name
+__NAMESPACE__ -> name of current namespace
+*/
+
+?>
---
+++ php_homework/tipsandtricks/magicconstants.phpuniqueid.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "This line of cpde is on line ". __LINE__; //should return line 3
+
+?>
