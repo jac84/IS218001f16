a7fe06470d3374539095a994a4ae4574d3623807
Jennifer A Causing <jac84@afsconnect1.njit.edu>
2016-12-15 17:40:18 -0500

    testing sanitation class

--- php_homework/homework10/valandsan.php
+++ php_homework/homework10/valandsan.php
@@ -9,8 +9,8 @@ filter_var(variable you want to check, type of check to use);
 can search for type of filters @ http://php.net/manual/en/filter.filters.php
 */
 
-//TODO: fix sanitize class after figuring out $_POST
-/*class EmailSan{
+
+class EmailSan{
 
   function sanitize(){
     if (isset($_POST['email'])){
@@ -19,25 +19,25 @@ can search for type of filters @ http://php.net/manual/en/filter.filters.php
     }
   }
 
-  function getPost(){
-    if(isset($_POST['submit'])) {
-      echo("Name: " . $_POST['name'] . "<br />\n");
-      echo("Email: " . $_POST['email'] . "<br />\n");
+  /*function getPost(){
+    if(isset($_POST['email']) && isset($_POST['name'])) {
+      echo $_POST['name']; //("Name: " . $_POST['name'] . "<br />\n");
+      echo $_POST['email'];//("Email: " . $_POST['email'] . "<br />\n");
    }
-  }
+ }*/
 
 }
 
 $san = new EmailSan;
-$san->getPost();*/
+$san->sanitize();
 
-if (isset($_POST['email'])){
+/*if (isset($_POST['email'])){
   $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL); //sanitizes email
   if(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
     echo $email. " is a valid email";
   } else {
     echo $email. " is NOT a valid email";
   }
-}
+}*/
 
 ?>
