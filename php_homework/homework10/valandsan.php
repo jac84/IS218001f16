658175765f1224dc99bd0fd1b4d9d34daceb2f35
Jennifer A Causing <jac84@afsconnect1.njit.edu>
2016-12-15 18:02:30 -0500

    created formval and formsan classes

--- php_homework/homework10/valandsan.php
+++ php_homework/homework10/valandsan.php
@@ -2,34 +2,65 @@
 
 /*
 filter_var -> sanitizes and validates form data
-sanitize -> remopve illegal chars from data
-validate -> determine if the data is in proper form
+sanitize -> remove illegal chars from data
+validate -> determine if the data is in proper form; returns boolean
 
 filter_var(variable you want to check, type of check to use);
 can search for type of filters @ http://php.net/manual/en/filter.filters.php
 */
 
 
-class EmailSan{
+class FormSan{
+
+  private $email;
+
+  public function __construct($e){
+    $this->email = $e;
+  }
 
   function sanitize(){
-    if (isset($_POST['email'])){
-      echo filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
-      echo "<br/><br/>";
+    echo "Email before sanitizing: ". $email. "<br />";
+    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+    echo "Email after sanitizing: ". $email. "<br />";
+    echo "<br/><br/>";
+  }
+
+  function getSanEmail(){
+    return filter_var($email, FILTER_SANITIZE_EMAIL);
+  }
+}
+
+class FormVal{
+
+  private $name;
+  private $email;
+
+  public function __construct($n, $e){
+    $this->name = $n;
+    $this->email = $e;
+  }
+
+  function validate(){
+    if(filter_var($name, FILTER_VALIDATE_STRING)){
+      echo $name. " is a valid name";
+    } else {
+      echo $name. " is NOT a valid name";
+    }
+
+    if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+      echo $email. " is a valid email";
+    } else {
+      echo $email. " is NOT a valid email";
     }
   }
 
-  /*function getPost(){
-    if(isset($_POST['email']) && isset($_POST['name'])) {
-      echo $_POST['name']; //("Name: " . $_POST['name'] . "<br />\n");
-      echo $_POST['email'];//("Email: " . $_POST['email'] . "<br />\n");
-   }
- }*/
-
 }
 
-$san = new EmailSan;
+$san = new FormSan($_POST['email']);
 $san->sanitize();
+
+$val = new FormVal($_POST['name'], $san->getSanEmail());
+$val->validate();
 
 /*if (isset($_POST['email'])){
   $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL); //sanitizes email
