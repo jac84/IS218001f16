038e2ada8112a95c57c6f28ba43208fb2882ea93
Jennifer A Causing <jac84@afsconnect1.njit.edu>
2016-12-19 00:28:24 -0500

    started fixing homework files and creating afs pages

--- php_homework/globals/_cookie.php
+++ php_homework/globals/_cookie.php
@@ -7,13 +7,14 @@ setcookie($cookie_name, $cookie_value, time(), (86400*30), "/"); //86400 = 1 day
 
 <html>
 <body>
+<h1>_cookie globals test</h1>
 
 <?php
 
 if(!isset($_COOKIE[$cookie_name])){
-  echo "Cookie named '" . $cookie_name . "' is not set!";
+  echo "Cookie named " . $cookie_name . " is not set!";
 } else {
-  echo "Cookie '" . $cookie_name . "' is set! <br>";
+  echo "Cookie " . $cookie_name . " is set! <br>";
   echo "Value is: " . $_COOKIE[$cookie_name];
 }
 
